b192a1c6c6328fecc3bd6d0a0620c47336a0634b
Abdellah Cherkaoui <cherkkaaoui.aabdellah@gmail.com>
2020-05-08 23:16:28 +0200

    updates

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,11 @@
 $(Document).ready(function(){
 
     $('[data-toggle="tooltip"]').tooltip();
+
+    $('[data-add-to-cart]').click(function(e){
+        alert('أضيف المنتج الى عربة الشراء');
+        e.stopPropagation();
+    });
 });
 
 
